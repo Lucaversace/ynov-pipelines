a7fcfc3b4e2fe0d4c29497ba9989f52972093d3c
Lucas Bento-Versace <lucas.bentoversace@ynov.com>
2023-01-23 12:30:05 +0100

    third fix yaml with black lint

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from bottle import route, run, template
 from functions import addition
 
 
-@route('/<name>')
+@route("/<name>")
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template("<b>Hello {{name}}</b>!", name=name)
 
 
-@route('/add/<a>/<b>/')
+@route("/add/<a>/<b>/")
 def add(a, b):
     return template(addition(a, b), a=a, b=b)
 
 
-run(host='localhost', port=8080)
+run(host="localhost", port=8080)
